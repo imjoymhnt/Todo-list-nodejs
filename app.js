7a3d227e2e208d360500abbf8ac01a64b2b03721
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-01-16 17:51:56 +0530

    complete work route

--- app.js
+++ app.js
@@ -8,16 +8,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const items = [];
+const workItems = [];
 
 app.get("/", (req, res) => {
   const day = date.getDate();
-  res.render("list", { listTitle: day, items });
+  res.render("list", { listTitle: day, items: items });
 });
 
 app.post("/", (req, res) => {
   const item = req.body.todo;
   items.push(item);
   res.redirect("/");
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", items: workItems });
+});
+
+app.post("/work", (req, res) => {
+  const item = req.body.todo;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, () => {
