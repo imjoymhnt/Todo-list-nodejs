87f3ab3eaec352eed6f446a94ee67c9699444b95
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-01-20 10:26:59 +0530

    connect to the mongodb database and add some default item

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -8,12 +8,40 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-const items = [];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Item = mongoose.model("item", itemsSchema);
+
+const item1 = new Item({
+  name: "Code",
+});
+const item2 = new Item({
+  name: "Eat",
+});
+const item3 = new Item({
+  name: "Sleep",
+});
+
+Item.insertMany([item1, item2, item3], (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Success");
+  }
+});
 
 app.get("/", (req, res) => {
-  const day = date.getDate();
-  res.render("list", { listTitle: day, items: items });
+  res.render("list", { listTitle: "Title", items: items });
 });
 
 app.post("/", (req, res) => {
